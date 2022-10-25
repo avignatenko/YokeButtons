2f034c2299ba55d224dfe77dbb7b1d6e5d21c2cb
Alexey Ignatenko <alexey.ignatenko@gmail.com>
2022-10-25 20:21:47 +0300

    Added one button as "shift" button, doubling total number of buttons in joystick

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,24 @@
 #include <Joystick.h>
 
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,
-                   JOYSTICK_TYPE_GAMEPAD, 6,
+                   JOYSTICK_TYPE_GAMEPAD, 19,
                    JOYSTICK_DEFAULT_HATSWITCH_COUNT,
                    false, false, false, false, false, false,
                    false, false, false, false, false);
-
-const int KEY_PORT_COUNT = 6;
 
 const int KEY_ARRAY_OFFSET[2] = {0, 8};
 const int KEY_PORT_BEGIN[2] = {2, A0};
 const int KEY_ROW_OFFSET[2] = {0, 4};
 const int KEY_COLUMN_OFFSET[2] = {2, 0};
 
+const int KEY_PORT_COUNT = 6;
+
 const int MAX_DEBOUNCE = 10;
 
-int buttons[16];
+const int SHIFT_BUTTON = 12;
+
+int buttons[16] = {0};
+bool shiftPressed = false;
 
 int btnPressed(int id)
 {
@@ -60,6 +63,25 @@ joyAction btn2joy[16] = {
     {true, 0, 0}     // 15
 };
 
+joyAction btn2joy_shift[16] = {
+    {true, 0 + 5, 0},  // 0
+    {true, 5 + 5, 0},  // 1
+    {true, 4 + 5, 0},  // 2
+    {true, 1 + 5, 0},  // 3
+    {true, 6 + 5, 0},  // 4
+    {true, 7 + 5, 0},  // 5
+    {true, 8 + 5, 0},  // 6
+    {true, 9 + 5, 0},  // 7
+    {true, 10 + 5, 0}, // 8
+    {true, 11 + 5, 0}, // 9
+    {true, 12 + 5, 0}, // 10
+    {true, 13 + 5, 0}, // 11
+    {true, 0, 0},      // 12 // 12 is SHIFT_BUTTON!
+    {true, 2 + 5, 0},  // 13
+    {true, 3 + 5, 0},  // 14
+    {true, 0 + 5, 0}   // 15
+};
+
 int getHatAngle(int hatId)
 {
   int hatDirs[4] = {-1, -1, -1, -1};
@@ -89,7 +111,10 @@ int getHatAngle(int hatId)
 
 void mapButtonToJoy(int changedBtn, int pressed)
 {
-  const joyAction &act = btn2joy[changedBtn];
+  if (changedBtn == SHIFT_BUTTON)
+    shiftPressed = pressed;
+
+  const joyAction &act = (shiftPressed ? btn2joy_shift[changedBtn] : btn2joy[changedBtn]);
   if (act.button)
   {
     if (pressed)
@@ -131,15 +156,15 @@ void loop()
 
           if (btnNewValue == 0)
           {
-            // Serial.print(btnIdx);
-            // Serial.println(" released!");
+            //Serial.print(btnIdx);
+            //Serial.println(" released!");
             mapButtonToJoy(btnIdx, 0);
           }
 
           if (btnNewValue == MAX_DEBOUNCE)
           {
-            // Serial.print(btnIdx);
-            // Serial.println(" pressed!");
+            //Serial.print(btnIdx);
+            //Serial.println(" pressed!");
             mapButtonToJoy(btnIdx, 1);
           }
         }
